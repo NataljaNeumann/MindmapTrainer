5682fbe8642241b756e458b97972c452a85dc178
NataljaNeumann <NataljaNeumann@gmx.de>
2025-04-05 13:17:05 +0200

    Added scripts for easier localization

---
+++ CreateTranslationCSVs.cmd
@@ -0,0 +1,5 @@
+ResxVsCsv --directory MindmapTrainer --pattern About*resx 
+ResxVsCsv --directory MindmapTrainer --pattern MindmapTrainerForm*resx 
+ResxVsCsv --directory MindmapTrainer --pattern SubjectTestForm*resx 
+ResxVsCsv --directory MindmapTrainer\Properties --pattern Resources*resx --addcultures af,ar,az,be,bg,bo-CN,bs-LatnBA,ca,cs,da,de,el,es,et,fa,fi,fr,he,hi,hr,hu,hy,id,is,it,ja,ka,kk,km-KH,ko,ky-KG,la-001,lt,lv,mk,mn-MN,ms,nl,no,pa-Arab-PK,pa-IN,pl,ps-AF,pt,ro,ru,sa,sk,sl,sr,sv,tg-Cyrl-TJ,th,tk-TM,tr,uk,uz,vi,zh-CHS,zh-CHT
+pause
---
+++ IntegrateTranslationCSVs.cmd
@@ -0,0 +1,5 @@
+ResxVsCsv --directory MindmapTrainer --toresx About.csv 
+ResxVsCsv --directory MindmapTrainer --toresx MindmapTrainerForm.csv 
+ResxVsCsv --directory MindmapTrainer --toresx SubjectTestForm.csv
+ResxVsCsv --directory MindmapTrainer\Properties --toresx Resources.csv 
+pause
